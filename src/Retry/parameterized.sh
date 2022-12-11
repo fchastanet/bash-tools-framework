00247e94d75a691c7654480b1b860373ff19cb39
François Chastanet <fchastanet@wiley.com>
2022-12-11 20:44:29 +0100

    fix shift issues

--- src/Retry/parameterized.sh
+++ src/Retry/parameterized.sh
@@ -8,10 +8,12 @@
 # @return 0 on success, 1 if max retries count reached
 Retry::parameterized() {
   local maxRetries=$1
-  local delayBetweenTries=$2
-  local message="$3"
+  shift || true
+  local delayBetweenTries=$1
+  shift || true
+  local message="$1"
+  shift || true
   local retriesCount=1
-  shift 3
   while true; do
     Log::displayInfo "Attempt ${retriesCount}/${maxRetries}: ${message}"
     if "$@"; then
