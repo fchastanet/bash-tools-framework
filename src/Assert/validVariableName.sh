537f31ee1c9bcd6d1d183eaaf5a44e1b172f9aa8
François Chastanet <fchastanet@wiley.com>
2023-01-14 18:09:13 +0100

    Assert::validVariableName stricter format

--- src/Assert/validVariableName.sh
+++ src/Assert/validVariableName.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 Assert::validVariableName() {
-  echo "$1" | grep -q '^[_[:alpha:]][_[:alpha:][:digit:]]*$' || return 1
+  echo "$1" | grep -E -q '^[A-Za-z_0-9:]+$' || return 1
 }
