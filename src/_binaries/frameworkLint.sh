afcc72b4ba19d25e18551b1bec25ed6c89fb2b98
François Chastanet <fchastanet@wiley.com>
2023-01-14 13:41:03 +0100

    frameworkLint - ability to lint external repository

--- src/_binaries/frameworkLint.sh
+++ src/_binaries/frameworkLint.sh
@@ -15,18 +15,23 @@ ${__HELP_TITLE}Usage:${__HELP_NORMAL} ${SCRIPT_NAME} [-h|--help] displays this h
 ${__HELP_TITLE}Usage:${__HELP_NORMAL} ${SCRIPT_NAME} [-f|--format <checkstyle,plain>]
 
 ${__HELP_TITLE}Description:${__HELP_NORMAL}
+Lint files of the current repository
 - check if all namespace::functions are existing in the framework
 - check that function defined in a .sh is correctly named
 
 ${__HELP_TITLE}Options:${__HELP_NORMAL}
   -f|--format <checkstyle,plain>  define output format of this command
+  --src-dir|-s <srcDir> provide the directory where to find the functions source code.
+      By default current src directory is used also.
 
 .INCLUDE "$(dynamicTemplateDir _includes/author.tpl)"
 EOF
 )"
 
 FRAMEWORK_FUNCTIONS_IGNORE_REGEXP=^$
-NON_FRAMEWORK_FILES_REGEXP=
+NON_FRAMEWORK_FILES_REGEXP=^$
+FRAMEWORK_FILES_FUNCTION_MATCHING_IGNORE_REGEXP=^$
+FRAMEWORK_SRC_DIRS=()
 if [[ -f "${CONFIG_FILENAME}" ]]; then
   # shellcheck source=/.framework-config
   source "${CONFIG_FILENAME}" || Log::fatal "error while loading config file '${CONFIG_FILENAME}'"
@@ -37,7 +42,7 @@ if (($# == 0)); then
 fi
 
 declare args
-args="$(getopt -l help,format: -o hf: -- "$@" 2>/dev/null)" || true
+args="$(getopt -l help,format:,src-dir: -o hs:f: -- "$@" 2>/dev/null)" || true
 eval set -- "${args}"
 
 while true; do
@@ -53,6 +58,13 @@ while true; do
       fi
       FORMAT="$1"
       ;;
+    --src-dir | -s)
+      shift || true
+      if [[ ! -d "$1" ]]; then
+        Log::fatal "Directory '$1' does not exists"
+      fi
+      FRAMEWORK_SRC_DIRS+=("$(realpath --physical "$1")")
+      ;;
     --)
       break
       ;;
@@ -62,6 +74,9 @@ while true; do
   esac
   shift || true
 done
+
+# add src dir by default
+FRAMEWORK_SRC_DIRS+=("${ROOT_DIR}/src")
 
 checkEachFunctionHasSrcFile() {
   local file="$1"
@@ -78,11 +93,18 @@ checkEachFunctionHasSrcFile() {
     fi
     local fileNameToImport
     fileNameToImport="$(echo "${functionToImport}" | sed -E 's#::#/#g').sh"
-    if [[ ! -f "${SRC_DIR}/${fileNameToImport}" ]]; then
+
+    local found=0
+    for srcDir in "${FRAMEWORK_SRC_DIRS[@]}"; do
+      if [[ -f "${srcDir}/${fileNameToImport}" ]]; then
+        found=1
+      fi
+    done
+    if [[ "${found}" = "0" ]]; then
       if [[ "${FORMAT}" = "plain" ]]; then
-        Log::displayError "The function ${functionToImport} called in ${file} does not have a matching source file ${fileNameToImport}"
+        Log::displayError "The function ${functionToImport} called in ${file} does not have a matching source file ${fileNameToImport} in any source directories specified"
       else
-        echo "<error severity='error' message='Function ${functionToImport} does not have a matching source file ${fileNameToImport}'/>"
+        echo "<error severity='error' message='Function ${functionToImport} does not have a matching source file ${fileNameToImport}  in any source directories specified'/>"
       fi
       return 1
     fi
@@ -92,7 +114,7 @@ checkEachFunctionHasSrcFile() {
 # search for at least one function that is matching the filename
 checkEachSrcFileHasOneFunctionCorrectlyNamed() {
   local srcFile="$1"
-  if [[ ! "${srcFile}" =~ ^src/ ]]; then
+  if echo "${srcFile}" | grep -q -E "${FRAMEWORK_FILES_FUNCTION_MATCHING_IGNORE_REGEXP}"; then
     return 0
   fi
   local foundFunctionMatching=0
