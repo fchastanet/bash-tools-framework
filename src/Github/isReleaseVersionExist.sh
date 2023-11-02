64fdf4a114b995b3e70b26775711a7f9885ce044
François Chastanet <fchastanet@wiley.com>
2023-11-02 20:39:15 +0100

    added Github::isReleaseVersionExist

---
+++ src/Github/isReleaseVersionExist.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# @description check if specified release software version exists in github
+# @arg $1 releaseUrl:String eg: https://github.com/kubernetes-sigs/kind/releases/download/v1.0.0/kind-linux-amd64
+# @exitcode 1 on failure
+# @exitcode 0 if release version exists
+Github::isReleaseVersionExist() {
+  local releaseUrl="$1"
+
+  curl \
+    -L \
+    -o /dev/null \
+    --silent \
+    --head \
+    --fail \
+    "${releaseUrl}"
+}
