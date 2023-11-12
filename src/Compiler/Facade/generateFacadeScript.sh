0344401ed9d7a0ce885f6f754911ca4d5f031406
François Chastanet <fchastanet@wiley.com>
2023-11-12 14:58:33 +0100

    facade script function name no more random, now based on script filename

--- src/Compiler/Facade/generateFacadeScript.sh
+++ src/Compiler/Facade/generateFacadeScript.sh
@@ -34,7 +34,7 @@ Compiler::Facade::generateFacadeScript() {
   Compiler::Facade::generateFacadeContent "${scriptFile}" >"${facadeContentTmpFile}" || return 2
 
   local mainFunctionName
-  mainFunctionName="$(Compiler::Facade::generateMainFunctionName)" || return 4
+  mainFunctionName="facade_main_$(basename "${scriptFile}" | sed -E -e 's/[^A-Za-z0-9]//g')" || return 4
 
   echo "export FACADE_HEADERS_FILE='${facadeHeadersTmpFile}'"
   echo "export FACADE_CONTENT_FILE='${facadeContentTmpFile}'"
