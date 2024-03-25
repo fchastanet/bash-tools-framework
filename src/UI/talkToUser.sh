bc5612da3dd1885f62cafb2c1d6786986677374f
François Chastanet <fchastanet@wiley.com>
2024-03-25 18:19:54 +0100

    UI::talkToUser allowed to execute custom powershell script(eg: embedded script)

--- src/UI/talkToUser.sh
+++ src/UI/talkToUser.sh
@@ -4,9 +4,11 @@
 # And wall Text to speech to tell the message if wsl and powershell available
 # Else try to use bip
 # @arg $1 msg:String message to tell
+# @arg $2 talkScript:String filepath to powershell talk script
 # @env CAN_TALK_DURING_INSTALLATION if not 1 skip talking or beeping
 UI::talkToUser() {
   local msg="${1:-Please, your input is required}"
+  local talkScript="${2-${FRAMEWORK_ROOT_DIR}/src/UI/talk.ps1}"
 
   if [[ "${CAN_TALK_DURING_INSTALLATION:-1}" != "1" ]]; then
     return
@@ -14,7 +16,8 @@ UI::talkToUser() {
 
   Log::displayInfo "${msg}"
   if Assert::wsl && "${BASH_FRAMEWORK_COMMAND:-command}" -v powershell.exe &>/dev/null; then
-    powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "${SCRIPTS_DIR}/talk.ps1" "'${msg}'" || true
+    ${POWERSHELL_BIN:-powershell.exe} -ExecutionPolicy Bypass -NoProfile -Command \
+      "$(Linux::Wsl::cachedWslpath -w "${talkScript}")" "'${msg}'" || true
   else
     tput bel || true
   fi
