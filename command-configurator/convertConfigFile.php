47dea8da212a7cb794ff6bd8b37fa16ece473667
François Chastanet <fchastanet@wiley.com>
2024-05-25 21:23:12 +0200

    enhanced config transformation + added json schema generation

--- command-configurator/convertConfigFile.php
+++ command-configurator/convertConfigFile.php
@@ -7,12 +7,11 @@ use Monolog\Logger;
 use Symfony\Component\Yaml\Yaml;
 use Opis\JsonSchema\{
   Validator,
-  ValidationResult,
   Errors\ErrorFormatter,
 };
 use Opis\JsonSchema\Errors\ValidationError;
-use Opis\JsonSchema\Exceptions\InvalidKeywordException;
 use Opis\JsonSchema\Exceptions\ParseException;
+use CommandConfigurator\Transformers\TransformConfigDefault;
 
 const YAML_INDENTATION = 2;
 const YAML_DUMP_LEVEL = PHP_INT_MAX;
@@ -46,41 +45,6 @@ function getConfig(Logger $logger, string $configFile): array {
   return require($configFile);
 }
 
-function transformConfig(Logger $logger, array &$config): void {
-  $logger->info("Transform config");
-  // check if each option has type set
-  if (is_array($config['binFile']['commands'])) {
-    foreach($config['binFile']['commands'] as &$command) {
-      if (is_array($command['options'])) {
-        foreach($command['options'] as &$option) {
-          if (!isset($option['type'])) {
-            $option['type'] = 'Boolean';
-          }
-          if (!isset($option['callbacks'])) {
-            $option['callbacks'] = [];
-          }
-          if (!isset($option['min'])) {
-            $option['min'] = 0;
-          }
-          if (!isset($option['max'])) {
-            $option['max'] = ($option['type'] === 'StringArray') ? -1 : 1;
-          }
-        }
-        unset($option);
-      }
-      if (is_array($command['args'])) {
-        foreach($command['args'] as &$option) {
-          if (!isset($option['callbacks'])) {
-            $option['callbacks'] = [];
-          }
-        }
-        unset($option);
-      }
-    }
-    unset($command);
-  }
-}
-
 function generateConfig(Logger $logger, string $configFile, array $config, string $targetConfigFile) {
   $value = Yaml::dump(
     $config,
@@ -101,7 +65,7 @@ function formatJsonSchemaParseError(Logger $logger, ValidationError $validationE
   }
 }
 
-function validateJson($jsonValidator, array $config, object $jsonSchema): bool {
+function validateJson(Logger $logger, $jsonValidator, array $config, object $jsonSchema): bool {
   $config = json_decode(json_encode($config));
   $result = $jsonValidator->validate($config, $jsonSchema);
   if (!$result->isValid()) {
@@ -112,22 +76,59 @@ function validateJson($jsonValidator, array $config, object $jsonSchema): bool {
   return true;
 }
 
+function generateJsonSchema(Logger $logger) {
+  $base = loadJsonFile($logger, __DIR__ . "/src/jsonSchema/commands.schema.template.json");
+  $args = loadJsonFile($logger, __DIR__ . "/src/jsonSchema/args.json");
+  $binFile = loadJsonFile($logger, __DIR__ . "/src/jsonSchema/binFile.json");
+  $commands = loadJsonFile($logger, __DIR__ . "/src/jsonSchema/commands.json");
+  $options = loadJsonFile($logger, __DIR__ . "/src/jsonSchema/options.json");
+  $parameter = loadJsonFile($logger, __DIR__ . "/src/jsonSchema/parameter.json");
+  $base['properties']['binFile'] = $binFile;
+  $base['properties']['binData']['properties']['commands'] = $commands;
+  $base['properties']['binData']['properties']['commands']['items']['properties']['options']['items'] = array_merge_recursive(
+    $parameter, $options
+  );
+  $base['properties']['binData']['properties']['commands']['items']['properties']['args']['items'] = array_merge_recursive(
+    $parameter, $args
+  );
+  file_put_contents(
+    __DIR__ . '/src/commands.schema.json',
+    json_encode($base, JSON_PRETTY_PRINT)
+  );
+}
+
+function loadJsonFile(Logger $logger, string $file) {
+  $logger->debug("Loading $file");
+  return json_decode(file_get_contents($file), true);
+}
+
 $logger = initLogger();
 $configFile = __DIR__ . "/config/shellcheckLint.php";
 $config = getConfig($logger, $configFile);
+print_r(
+  $config);
 $intermediateConfigFile = __DIR__ . "/config-generated/shellcheckLint.beforeTransform.json";
 file_put_contents(
   $intermediateConfigFile,
   json_encode($config, JSON_PRETTY_PRINT)
 );
 $logger->info("Written intermediate json file before transform $intermediateConfigFile");
-transformConfig($logger, $config);
 
+$intermediateConfigFile = __DIR__ . "/config-generated/shellcheckLint.afterTransform.json";
+$transformConfig = new TransformConfigDefault($logger);
+$transformConfig->transformConfig($config);
+file_put_contents(
+  $intermediateConfigFile,
+  json_encode($config, JSON_PRETTY_PRINT)
+);
+$logger->info("Written intermediate json file after transform $intermediateConfigFile");
+
+generateJsonSchema($logger);
 $jsonValidator = initJsonSchemaValidator();
 $jsonSchema = json_decode(
-  file_get_contents(__DIR__.'/schema/commands.schema.json')
+  file_get_contents(__DIR__.'/src/commands.schema.json')
 );
-if (!validateJson($jsonValidator, $config, $jsonSchema)) {
+if (!validateJson($logger, $jsonValidator, $config, $jsonSchema)) {
   exit(1);
 }
 try {
