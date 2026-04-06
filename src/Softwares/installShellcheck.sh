cffbf6fb01065e6eb0b04e68b56a708cce335743
Francois Chastanet <237869+fchastanet@users.noreply.github.com>
2026-04-06 19:33:25 +0200

    ✨(shellcheck): ensure shellcheck binary is installed and up to date +new Format::duration

--- src/Softwares/installShellcheck.sh
+++ src/Softwares/installShellcheck.sh
@@ -2,9 +2,11 @@
 
 # @description install hadolint if necessary
 # @arg $1 targetFile:String
+# @arg $2 maxAge:int in seconds, default to 1 month
 # @feature Github::upgradeRelease
 Softwares::installShellcheck() {
   local targetFile="${1:-${FRAMEWORK_VENDOR_BIN_DIR}/shellcheck}"
+  local maxAge=${2:-$((30 * 24 * 3600))} # 1 month in seconds
   # shellcheck disable=SC2329
   install() {
     local file="$1"
@@ -17,10 +19,21 @@ Softwares::installShellcheck() {
       tar -xJvf "${file}" >&2
       mv "shellcheck-v${version}/shellcheck" "${targetFile}"
       chmod +x "${targetFile}"
+      touch -d "now" "${targetFile}"
       rm -f "${file}" || true
     )
   }
+  local fileAge
+  fileAge=$(File::elapsedTimeSinceLastModification "${targetFile}")
+  if ((fileAge < maxAge)); then
+    Log::displayInfo "Using cached shellcheck (age: $(Format::duration "${fileAge}"))"
+    return 0
+  fi
+  Log::displayInfo "Shellcheck is older than 1 month, re-downloading..."
   INSTALL_CALLBACK=install Github::upgradeRelease \
     "${targetFile}" \
     "https://github.com/koalaman/shellcheck/releases/download/v@latestVersion@/shellcheck-v@latestVersion@.linux.x86_64.tar.xz"
+
+  # ensure the timestamp is updated even if the file was already up to date to prevent repeated downloads
+  touch -d "now" "${targetFile}"
 }
